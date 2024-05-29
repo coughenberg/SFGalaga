122f0740e4ea0aa71ebd6d584060826abd46b4a6
coughenberg <khfantk@gmail.com>
2024-05-28 20:10:20 -0500

    feat: animations of ship muzzle and asteroids

--- asteroids/asteroids.gd
+++ asteroids/asteroids.gd
@@ -3,6 +3,11 @@ class_name Asteroid extends Area2D
 # total health of the asteroid, adjusted by the children
 @export var health = 2
 
+# animation of the explosion
+@onready var animate_explosion: AnimatedSprite2D = $AnimateExplosion
+
+const finish_animate = .15
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -14,11 +19,13 @@ func asteroid_exited_screen():
 	queue_free()
 
 func die():
+	animate_explosion.play("explosion")
+	await get_tree().create_timer(finish_animate).timeout
 	queue_free()
 
 func damage():
 	health -= 1
-	if health <= 0:
+	if health == 0:
 		die()
 
 func on_body_entered(body):
